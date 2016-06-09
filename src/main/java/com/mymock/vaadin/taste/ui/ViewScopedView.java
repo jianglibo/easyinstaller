375dbd46d88b503b298331286ada54dd9a40ce99
libo <jianglibo@gmail.com>
2016-06-09 22:18:30 +0800

    init

---
+++ src/main/java/com/mymock/vaadin/taste/ui/ViewScopedView.java
@@ -0,0 +1,40 @@
+package com.mymock.vaadin.taste.ui;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+@SpringView(name = ViewScopedView.VIEW_NAME)
+public class ViewScopedView extends VerticalLayout implements View {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -3675894053864446559L;
+    public static final String VIEW_NAME = "view";
+    
+    @Autowired
+    private ViewGreeter viewGreeter;
+    
+    @Autowired
+    private Greeter uiGreeter;
+
+    @PostConstruct
+    void init() {
+        setMargin(true);
+        setSpacing(true);
+        addComponent(new Label("This is a view scoped view"));
+        addComponent(new Label(uiGreeter.sayHello()));
+        addComponent(new Label(viewGreeter.sayHello()));
+    }
+
+    @Override
+    public void enter(ViewChangeEvent event) {
+        // the view is constructed in the init() method()
+    }
+}
