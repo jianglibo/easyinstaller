950eccaf13acaca4f2eea9f9ac437e85c681713b
jianglibo <jianglibo@gmail.com>
2016-08-19 11:11:28 +0800

    about to implement own datacontainer

---
+++ src/main/java/com/jianglibo/vaadin/dashboard/container/FreeContainer.java
@@ -0,0 +1,156 @@
+package com.jianglibo.vaadin.dashboard.container;
+
+import java.util.Collection;
+import java.util.List;
+
+import com.vaadin.data.Container;
+import com.vaadin.data.Item;
+import com.vaadin.data.Property;
+import com.vaadin.data.util.AbstractContainer;
+
+@SuppressWarnings("serial")
+public class FreeContainer extends AbstractContainer implements Container.Indexed, Container.Sortable {
+
+	@Override
+	public Object nextItemId(Object itemId) {
+		return null;
+	}
+
+	@Override
+	public Object prevItemId(Object itemId) {
+		return null;
+	}
+
+	@Override
+	public Object firstItemId() {
+		return null;
+	}
+
+	@Override
+	public Object lastItemId() {
+		return null;
+	}
+
+	@Override
+	public boolean isFirstId(Object itemId) {
+		return false;
+	}
+
+	@Override
+	public boolean isLastId(Object itemId) {
+		return false;
+	}
+
+	@Override
+	public Object addItemAfter(Object previousItemId) throws UnsupportedOperationException {
+		return null;
+	}
+
+	@Override
+	public Item addItemAfter(Object previousItemId, Object newItemId) throws UnsupportedOperationException {
+		return null;
+	}
+
+	@Override
+	public Item getItem(Object itemId) {
+		return null;
+	}
+
+	@Override
+	public Collection<?> getContainerPropertyIds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<?> getItemIds() {
+		return null;
+	}
+
+	@Override
+	public Property getContainerProperty(Object itemId, Object propertyId) {
+		return null;
+	}
+
+	@Override
+	public Class<?> getType(Object propertyId) {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public boolean containsId(Object itemId) {
+		return false;
+	}
+
+	@Override
+	public Item addItem(Object itemId) throws UnsupportedOperationException {
+		return null;
+	}
+
+	@Override
+	public Object addItem() throws UnsupportedOperationException {
+		return null;
+	}
+
+	@Override
+	public boolean removeItem(Object itemId) throws UnsupportedOperationException {
+		return false;
+	}
+
+	@Override
+	public boolean addContainerProperty(Object propertyId, Class<?> type, Object defaultValue)
+			throws UnsupportedOperationException {
+		return false;
+	}
+
+	@Override
+	public boolean removeContainerProperty(Object propertyId) throws UnsupportedOperationException {
+		return false;
+	}
+
+	@Override
+	public boolean removeAllItems() throws UnsupportedOperationException {
+		return false;
+	}
+
+	@Override
+	public void sort(Object[] propertyId, boolean[] ascending) {
+		
+	}
+
+	@Override
+	public Collection<?> getSortableContainerPropertyIds() {
+		return null;
+	}
+
+	@Override
+	public int indexOfId(Object itemId) {
+		return 0;
+	}
+
+	@Override
+	public Object getIdByIndex(int index) {
+		return null;
+	}
+
+	@Override
+	public List<?> getItemIds(int startIndex, int numberOfItems) {
+		return null;
+	}
+
+	@Override
+	public Object addItemAt(int index) throws UnsupportedOperationException {
+		return null;
+	}
+
+	@Override
+	public Item addItemAt(int index, Object newItemId) throws UnsupportedOperationException {
+		return null;
+	}
+
+}
