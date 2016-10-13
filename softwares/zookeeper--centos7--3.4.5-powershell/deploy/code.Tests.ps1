850609e24ae7df520956a60e6beb6c76a011e651
libo <jianglibo@gmail.com>
2016-10-13 21:01:06 +0800

    more powershell

--- softwares/zookeeper--centos7--3.4.5-powershell/deploy/code.Tests.ps1
+++ softwares/zookeeper--centos7--3.4.5-powershell/deploy/code.Tests.ps1
@@ -11,6 +11,12 @@ Describe "code" {
     It "should be HashTable" {
         $clusterCfg.EnvForCodeExec.boxGroup.boxes | foreach {@{ip=$_.ip; hostname=$_.hostname}} | Get-Member | select -ExpandProperty TypeName|  Should Be "System.Collections.Hashtable"
     }
+    It "should still be XmlElement" {
+        $clusterCfg.EnvForCodeExec.boxGroup.boxes | Select-Object ip, hostname, @{name="serverId"; expression={$_.ip.split(".")[-1]}} | Get-Member | select -ExpandProperty TypeName|  Should Be "Selected.System.Xml.XmlElement"
+    }
+    It "should get scalar value" {
+        $clusterCfg.EnvForCodeExec.boxGroup.boxes | Select-Object ip, hostname, @{name="serverId"; expression={$_.ip.split(".")[-1]}} | Select-Object -First 1 -ExpandProperty serverId | Should Be "10"
+    }
     It "should format string" {
         $ht = @{str="abc"; int=55}
         "$($ht.str)xxx" | Should Be "abcxxx"
