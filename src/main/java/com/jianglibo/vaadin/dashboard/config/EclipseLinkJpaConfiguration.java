622ec804062ee5c7db5c918cf7b2db31fd45d057
jianglibo <jianglibo@gmail.com>
2017-01-07 11:37:15 +0800

    update dependencies

---
+++ src/main/java/com/jianglibo/vaadin/dashboard/config/EclipseLinkJpaConfiguration.java
@@ -0,0 +1,36 @@
+package com.jianglibo.vaadin.dashboard.config;
+
+import java.util.Collections;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+@Configuration
+public class EclipseLinkJpaConfiguration extends JpaBaseConfiguration {
+
+	@Autowired
+	protected EclipseLinkJpaConfiguration(DataSource dataSource, JpaProperties properties,
+			ObjectProvider<JtaTransactionManager> jtaTransactionManagerProvider) {
+		super(dataSource, properties, jtaTransactionManagerProvider);
+	}
+
+	@Override
+	protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+		return new EclipseLinkJpaVendorAdapter();
+	}
+
+	@Override
+	protected Map<String, Object> getVendorProperties() {
+		return Collections.singletonMap("eclipselink.weaving", "false");
+	}
+
+}
--- src/main/java/com/jianglibo/vaadin/dashboard/domain/BaseEntity.java
+++ src/main/java/com/jianglibo/vaadin/dashboard/domain/BaseEntity.java
@@ -27,7 +27,7 @@ public abstract class BaseEntity implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.TABLE)
-	private long id;
+	private Long id;
 
 	@Version
 	private int version;
@@ -59,11 +59,11 @@ public abstract class BaseEntity implements Serializable {
 		}
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
